043611833732c50f9b9e90e801ee1ba3a07e7d12
Carlos Santos <Carlos Santos@LAPTOP-H2K9SUQI>
2022-03-12 16:26:44 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio01/Ejercicio01.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio01
+{
+    class Ejercicio01
+    {
+        static void Main(string[] args)
+        {
+            //1. Crear 3 variables numericas con el valor que tu quieras y en otra variable númerica
+            //guardar el valor de la suma de las 3 anteriores.Mostrar por consola.
+
+            int num1 = 18, num2 = 10, num3 = 8, resultado;
+
+            resultado = num1 + num2 + num3;
+
+            Console.WriteLine("La Suma de todos los numeros es: " + resultado);
+
+
+
+        }
+    }
+}
