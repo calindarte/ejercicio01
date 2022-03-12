719f864cbf4e779f7f2d7a04c83d33014aefeb31
calindarte <101478563+calindarte@users.noreply.github.com>
2022-03-12 16:32:23 -0500

    Update Class1.cs

--- Ejercicio02/Class1.cs
+++ Ejercicio02/Class1.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Ejercicio02
 {
-    internal class Class1
+    class Class1
     {
+         static void Main()
+        {  /*2.Pedir por consola un nombre de persona y el nombre de una ciudad(no hace falta 
+            que sean reales o comprobarlos) y mostrar por pantalla, el siguiente mensaje "Hola " nombre " 
+            bienvenido a " ciudad*/
+
+            string name, city;
+
+            Console.WriteLine("Estimado usuario, ingrese su nombre por favor :)");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Ahora ingrese la ciudad en donde reside :)");
+            city = Console.ReadLine();
+
+            Console.WriteLine("Hola " + name + " Bienvenido a " + city);
+            Console.ReadLine();
     }
 }
